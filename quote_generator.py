54810129ba1b44d61cf12e462176aca3dccdba6d
charlesnoah344 <23397@ecam.be>
2025-06-30 20:53:16 +0200

    erreur dans le process evnts

--- quote_generator.py
+++ quote_generator.py
@@ -18,6 +18,41 @@ def afficher(text,position,taille,police,couleur):
     clock = pygame.time.Clock()
     return (text_surface, text_rect)
 
+def draw_button(screen, rect, text, font, idle_color, hover_color, text_color, border_radius=8):
+    """
+    Dessine un bouton et renvoie True si cliqué.
+
+    :param screen: surface pygame
+    :param rect: pygame.Rect(x, y, width, height)
+    :param text: texte du bouton
+    :param font: pygame.font.Font
+    :param idle_color: couleur normale (tuple RGB)
+    :param hover_color: couleur au survol (tuple RGB)
+    :param text_color: couleur du texte (tuple RGB)
+    :param border_radius: arrondi du bouton
+    :return: True si cliqué, False sinon
+    """
+    mouse_pos = pygame.mouse.get_pos()
+    clicked = False
+
+    # Détecter le survol
+    if rect.collidepoint(mouse_pos):
+        color = hover_color
+        if pygame.mouse.get_pressed()[0]:  # bouton gauche
+            clicked = True
+    else:
+        color = idle_color
+
+    # Dessiner le bouton
+    pygame.draw.rect(screen, color, rect, border_radius=border_radius)
+
+    # Dessiner le texte
+    text_surf = font.render(text, True, text_color)
+    text_rect = text_surf.get_rect(center=rect.center)
+    screen.blit(text_surf, text_rect)
+
+    return clicked
+
 class App:
   def __init__(self):
     pygame.init()
@@ -28,11 +63,14 @@ class App:
     self.auteur=''
     self.display_position=(400,150)
 
-    self.play_button = UIButton(
+    '''self.play_button = UIButton(
       relative_rect=pygame.Rect(330, 235, 150, 50),
       text='Play',
       manager=self.manager
-    )
+    )'''
+    self.play_button=pygame.Rect(330, 235, 150, 50)
+    self.font = pygame.font.SysFont(None, 36)
+
 
     self.display_label=afficher(f'{self.citation}',self.display_position,20,'Comic Sans MS',(0, 0, 0))#couleur blanche
     self.screen.blit(self.display_label[0], self.display_label[1])
@@ -49,8 +87,13 @@ class App:
                     return (author, quote)
                     
   def process_events(self, event: pygame.event.Event):
-    if event.type == pygame_gui.UI_BUTTON_PRESSED:
-      if event.ui_element is self.play_button:
+    '''if event.type == pygame_gui.UI_BUTTON_PRESSED:
+      if event.ui_element is self.play_button:'''
+    if self.play_button(self.screen, self.play_button, "Clique-moi", self.font,
+                       idle_color=(70, 130, 180),
+                       hover_color=(100, 160, 210),
+                       text_color=(255, 255, 255),
+                       border_radius=12):
         quotes=self.random_quote()
         self.citation=f'{quotes[1]}\n {quotes[0]}'
         self.display_label=afficher(f'{self.citation}',self.display_position,20,'Comic Sans MS',(0, 0, 0))#couleur blanche
